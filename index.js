6d7bc72b589d11b58c02260b8baf547c08014e87
hasan469328 <hasanur.68.bd@gmail.com>
2023-05-27 08:21:35 +0600

    basic server created

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-const app = express()
-const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT | 5000
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.3vyivuy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,28 +17,42 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("bossDB").collection("menu");
+    const reviewCollection = client.db("bossDB").collection('review')
+
+    app.get('/menu', async (req,res)=>{
+      const result = await menuCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/reviews', async(req,res)=> {
+      const result = await reviewCollection.find().toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req,res)=> {
-  res.send('boss is running')
-})
+app.get("/", (req, res) => {
+  res.send("boss is running");
+});
 
 app.listen(port, () => {
-  console.log(`boss is running on port: ${port}`)
-})
+  console.log(`boss is running on port: ${port}`);
+});
